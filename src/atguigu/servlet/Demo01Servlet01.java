981fb3ec98dc4356fc184a65d092f829611ad72a
ycy <ycy@ycy.com>
2021-08-15 00:57:03 +0800

    克隆下来的项目的第一次提交

--- src/atguigu/servlet/Demo01Servlet01.java
+++ src/atguigu/servlet/Demo01Servlet01.java
@@ -15,6 +15,7 @@ public class Demo01Servlet01 extends HttpServlet {
         System.out.println("我是master分支,主干分支");
         System.out.println("我直接在gitee上修改");
         System.out.println("本地修改 push上gitee");
+        System.out.println("我是克隆后的项目 本地提交修改");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
